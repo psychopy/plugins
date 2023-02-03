48a3e368f9d1bfddfb39e7b7f2bf3b3b47f45858
Todd Parsons <todd.e.parsons@googlemail.com>
2023-02-03 13:39:15 +0000

    Remove install button from nav bar

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ napoleon_include_special_with_doc = False
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme_path = ["themes"]
-html_theme = 'psychopy_bootstrap'
+html_theme = 'psychopy_plugin'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
