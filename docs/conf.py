7642cbbc1f82ac711528fb720d2bebefb8a53849
Todd OST <todd.e.parsons@gmail.com>
2025-04-25 17:04:33 +0100

    DOC: Fix blurry images in plugin grid

--- docs/conf.py
+++ docs/conf.py
@@ -163,6 +163,9 @@ html_favicon = '_static/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'css/plugingrid.css'
+]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
